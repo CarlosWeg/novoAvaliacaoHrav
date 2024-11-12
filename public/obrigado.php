ba3f9f76be0daa6890014699a9665baf0ec2dc13
Carlos <calososla@gmail.com>
2024-11-12 11:18:19 -0300

    leves alteracoes

--- public/obrigado.php
+++ public/obrigado.php
@@ -8,12 +8,17 @@
     <script src="js/scripts.js"></script>
 </head>
 <body>
+    
 
     <div class = "agradecimento-container" id = "pagina-agradecimento">
+
+        <img src = "css/img/logo-white.png" alt = "Logo do Hospital" id = "logo-hospital">
+        
 
         <h1>Muito obrigado!</h1>
         <p>O Hospital Regional Alto Vale (HRAV) agradece sua resposta e ela é muito importante para nós, pois nos ajuda a melhorar continuamente nossos serviços.</p>
         <p>Você será redirecionado para a página inicial em 5 segundos.</p>
+        
 
     </div>
 
